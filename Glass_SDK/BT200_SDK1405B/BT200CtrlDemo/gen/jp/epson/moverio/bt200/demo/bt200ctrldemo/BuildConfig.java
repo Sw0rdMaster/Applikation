32b1a520604dff5c1e21f321e3e650b67d0e7c26
Roman Kieser <romankieser@hotmail.com>
2015-10-21 16:40:35 +0200

    Anfangsstadion Applikation

---
+++ Glass_SDK/BT200_SDK1405B/BT200CtrlDemo/gen/jp/epson/moverio/bt200/demo/bt200ctrldemo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package jp.epson.moverio.bt200.demo.bt200ctrldemo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
